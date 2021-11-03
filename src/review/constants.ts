3cf08fa2b776be484507dfa44c06c508be381c17
omgpiu <dyba@List.ru>
2021-11-03 20:46:08 +0300

    Создание сервиса

---
+++ src/review/constants.ts
@@ -0,0 +1,1 @@
+export const REVIEW_NOT_FOUND = "Отзвы с таким id не найден";
--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,5 +1,6 @@
 import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
 import { prop } from "@typegoose/typegoose";
+import { Types } from "mongoose";
 
 export interface ReviewModel extends Base {
 
@@ -15,5 +16,5 @@ export class ReviewModel extends TimeStamps {
   @prop()
   rating: number;
   @prop()
-  createdAt: Date;
+  productId: Types.ObjectId;
 }
